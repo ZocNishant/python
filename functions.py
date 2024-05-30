4efd52357240283edf4ea98a9caec092415133a7
Zoc Nishant <zocnishant@gmail.com>
2024-05-30 21:07:11 +0545

    strings, functions

---
+++ functions.py
@@ -0,0 +1,10 @@
+def evenOdd(a):
+    if a % 2 == 0:
+        return("Even")
+    else:
+        return("Odd")
+
+
+result = evenOdd(15)
+
+print(result)
---
+++ strings.py
@@ -0,0 +1,3 @@
+name = "Nishant Joshi"
+
+print(name[0])
