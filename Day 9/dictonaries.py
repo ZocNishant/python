03dc1674434be60c4df4594f9b6c9668ab1af4af
Zoc Nishant <zocnishant@gmail.com>
2024-06-25 22:30:57 +0545

    Day 9

---
+++ Day 9/dictonaries.py
@@ -0,0 +1,11 @@
+# {key: value}
+
+progamming_dictionary = {
+    "Nepal":"Kathmandu",
+    "Pasa":"Tei xa",
+    "Nishant": "Joshi",
+}
+
+progamming_dictionary["Loop"] = "Passa"
+
+print(progamming_dictionary)
